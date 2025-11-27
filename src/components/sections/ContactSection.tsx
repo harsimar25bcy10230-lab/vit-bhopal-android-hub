d9a11c743e3dd189e68768fa225a94daeb1aa098
harsimar25bcy10230-lab <harsimar.25bcy10230@vitbhopal.ac.in>
2025-11-27 20:17:35 +0530

    Update ContactSection.tsx

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -6,14 +6,14 @@ const ContactSection = () => {
     {
       name: "Instagram",
       icon: Instagram,
-      url: "https://instagram.com/androidclubvitb",
+      url: "https://www.instagram.com/androidclub.vitb/",
       color: "from-pink-500 to-orange-500",
       handle: "@androidclubvitb",
     },
     {
       name: "LinkedIn",
       icon: Linkedin,
-      url: "https://linkedin.com/company/androidclubvitb",
+      url: "https://www.linkedin.com/company/android-club-vit-bhopal/",
       color: "from-blue-600 to-blue-400",
       handle: "/company/androidclubvitb",
     },
